d5196705e9b2b54340a476ac59dd405f8eb62460
viasola <foongyuan22@gmail.com>
2022-07-13 14:11:29 +0800

    added brand header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
 import { TiThMenu } from "react-icons/ti";
 import { GiPineTree } from "react-icons/gi";
+import { MdLogout } from "react-icons/md";
 import './Header.css'
 
 export default function Header() {
   return (
     <>
+    <header style={{backgroundColor:"mediumseagreen", textAlign:"left"}}>
+      <h1 className="brand-name" style={{marginBottom:"0px", paddingLeft:"50px", paddingTop:"20px", paddingBottom:"20px" }}>MONEY TREE <MdLogout style={{float: "right", marginRight:"50px"}}/></h1>
+      
+    </header>
 
       <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
        
@@ -22,18 +27,15 @@ export default function Header() {
             <div class="dropdown-menu" >
               <a class="dropdown-item" href="#/expenses">Expenses</a>
               <a class="dropdown-item" href="#/incomes">Incomes</a>
-              <a class="dropdown-item" href="#/">LOG OUT</a>
-
               <a class="dropdown-item" href="#/transaction-report">Transaction Report</a>
-              
+              <a class="dropdown-item" href="#/">Log Out</a>
             </div>
           </li>
         </ul>
 
         {/* <div className="login-status"><h6>LOGGED IN</h6></div> */}
       </nav>
-
-    
+   
     </>
   )
 }
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -52,13 +52,13 @@ export default function Budget () {
   return (
     <>
       <div className="budget" style={{width:"60%", textAlign:"left", marginTop:"60px",marginLeft:"150px"}}>
-        <h3>Current budget balance : RM{budget - expense <= 0? 0 : (budget - expense)} left <span style={{float:"right"}}><EditBudget budget={budget} setBudget={setBudget} updateBudget={updateBudget} id={id} className="edit-btn"/></span></h3>
+        <h3>Current budget balance : <span style={{fontWeight:"bold"}}>RM{budget - expense <= 0? 0 : (budget - expense)}</span> left <span style={{float:"right"}}><EditBudget budget={budget} setBudget={setBudget} updateBudget={updateBudget} id={id} className="edit-btn"/></span></h3>
         
         
         <div className="bar-wrapper" style={{width:'100%',height:'37px',border:'solid', backgroundColor:'gainsboro'}} >
           <div className="bar" style={{width: (budget-expense) <= 0 ? "0%" : `${100- (expense/budget*100)}%`,height:'30px',backgroundColor:"gold"}} ></div>
         </div>
-        <h5>RM{budget === 0 ? 0 : expense} / RM{budget}</h5>
+        <h5><span style={{color:"firebrick"}}>RM{budget === 0 ? 0 : expense}</span> of <span style={{color:"forestgreen"}}>RM{budget}</span> budget spent</h5>
         
       </div>
     </>
