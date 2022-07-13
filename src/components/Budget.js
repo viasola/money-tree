76e57a92e398443023982168227dcc1d9fd741c7
xin zhao <xinzhao@live.com.my>
2022-07-13 10:55:53 +0800

    updated budget database

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,7 +4,9 @@ import EditBudget from "./EditBudget"
 export default function Budget () {
 
   let [expense, setExpense] = useState([])
+  
   let [budget, setBudget] = useState(0)
+  let [id,setId] = useState('')
 
   useEffect(() => {
       fetch(`http://localhost:8080/expenses/monthly/7`)
@@ -14,16 +16,47 @@ export default function Budget () {
        .then(data => setExpense(data))
   }, [])
 
-   
+  const updateBudget = async(budget_amount,budget) => {
+    try {
+      const body = {budget_amount}
+      const response = await fetch(`http://localhost:8080/budget/${id}`,{
+        method:"PUT",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(body)
+      })
+      getBudget()
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  function getBudget() {
+    fetch('http://localhost:8080/budget')
+    .then(res => res.json())
+    // .then(data => setData(data))
+    .then(data => {
+      data.map(dat => setBudget(dat.budget_amount))
+      data.map(dat => setId(dat.id))
+
+    }) 
+  }
+
+  useEffect(() => {
+   getBudget()
+      
+  },[])
+
+  
+
     
   return (
     <>
       <div className="budget" style={{width:"60%", textAlign:"left", marginTop:"60px",marginLeft:"150px"}}>
-        <h3>Current budget balance : RM{budget - expense <= 0? 0 : (budget - expense)} left <span style={{float:"right"}}><EditBudget budget={budget} setBudget={setBudget} className="edit-btn"/></span></h3>
+        <h3>Current budget balance : RM{budget - expense <= 0? 0 : (budget - expense)} left <span style={{float:"right"}}><EditBudget budget={budget} setBudget={setBudget} updateBudget={updateBudget} id={id} className="edit-btn"/></span></h3>
         
         
-        <div className="bar-wrapper" style={{width:'100%',height:'37px',border:'solid', backgroundColor:'gainsboro'}} min='0'>
-          <div className="bar" style={{width: budget <= 0 ? '0%' : `${100 - (expense / budget * 100)}%`,height:'30px',backgroundColor:"gold"}} min='0'></div>
+        <div className="bar-wrapper" style={{width:'100%',height:'37px',border:'solid', backgroundColor:'gainsboro'}} >
+          <div className="bar" style={{width: (budget-expense) <= 0 ? "0%" : `${100- (expense/budget*100)}%`,height:'30px',backgroundColor:"gold"}} ></div>
         </div>
         <h5>RM{budget === 0 ? 0 : expense} / RM{budget}</h5>
         
--- src/components/EditBudget.js
+++ src/components/EditBudget.js
@@ -1,53 +1,43 @@
+import { useState } from "react"
 
-export default function EditBudget(props) {
+export default function EditBudget({budget,setBudget,updateBudget,id}) {
 
-// const handleChange = (e) => {
-//     e.preventDefault()
-//     setBudgetInput( )
-// }
-    return (
-<>
-<div class="container">
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-    Edit
-  </button>
+  const [budget_amount,setAmount] = useState()
 
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-      
-        <div class="modal-header">
-          <h4 class="modal-title">Enter Budget</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+  return (
+  <>
+    <div class="container">
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target={`#id${id}`}>
+        Edit
+      </button>
+
+      <div class="modal" id={`id${id}`}>
+        <div class="modal-dialog">
+          <div class="modal-content">
+          
+            <div class="modal-header">
+              <h4 class="modal-title">Enter Budget</h4>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            
+            <div class="modal-body">
+              <input type="number" value={budget_amount} onChange={e => setAmount(Number(e.target.value))}/>
+            </div>
+            
+            <div class="modal-footer">
+                <button class="btn btn-warning" data-dismiss="modal" onClick={() => updateBudget(Number(budget_amount),budget)}>Update</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+            
+          </div>
         </div>
-        
-        <div class="modal-body">
-          <input type="number" value={props.budget} onChange={e => props.setBudget(e.target.value)}/>
-        </div>
-        
-        <div class="modal-footer">
-            <button class="btn btn-warning" data-dismiss="modal">Update</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        </div>
-        
       </div>
+      
     </div>
-  </div>
-  
-</div>
 
 
-
-        {/* <div>
-            <button className="edit-btn">Edit</button>
-            <form action="">
-                <input type={budgetInput ? 'text' : 'hidden'} />
-                <button>Update</button>
-            </form>
-        </div> */}
-
-
-</>
+  </>
         
-    )
+  )
 }
